705d8419deb0ba05df7a0db93ee771fc40951c04
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-30 20:56:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_key = ""
-  new_value = name_hash.each { |value| puts value }     
+  new_value = name_hash.each { |value| puts value }
     name_hash.each do |key, value|
       until value < new_value
         key
       end
-    end    
+    end
   end
 end
